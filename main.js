d8038ec3d5bd2899b45ac24473add0fc67f674b6
Gideon Akunana <gideonakunana@gmail.com>
2021-02-20 10:21:50 +0100

    added mousemove event

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-// Selects all inputs on the html
-
+// Gets all inputs on the html
 const inputs = document.querySelectorAll('.controls input');
 
 // Returns a NODELIST which is similar to an ARRAY but different
@@ -8,9 +7,12 @@ const inputs = document.querySelectorAll('.controls input');
 // Log input value
 function handleUpdate() {
     console.log(this.value);
+    /*  
+        this.dataset is an object that will contain all of the  data attributes from a specific element
+        Returns an empty object when there is no data attribute found
+    */
 }
 
-// Add event listener to all inputs whenever mouse click and moves
-inputs.forEach(input => {
-    input.addEventListener('change', handleUpdate);
-})
+// Add event listener to all inputs whenever range changes and mouse moves
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
